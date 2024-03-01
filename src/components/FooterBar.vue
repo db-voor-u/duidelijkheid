80d453849985366871c3b98ffbfbdfa7c03e128a
JeanPierre Meijer <duidelijkheidinbeeld@outlook.com>
2024-03-01 22:12:29 +0100

    image vijfde

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -3,7 +3,7 @@
 
      <div class="w-full h-10 bottom-0 border-t-2 bg-primary-color border-secondary-color">
         <div class="">
-            <p class="text-center text-xs mt-2">duidelijkheid@2024</p>
+            <p class="text-center text-xs mt-2">duidelijkheid@2024 🛠️</p>
         </div>
 
      </div>
