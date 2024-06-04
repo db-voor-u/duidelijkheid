68ce03655506e1f29349f4518337d76763edd2c9
JeanPierre Meijer <duidelijkheidinbeeld@outlook.com>
2024-06-05 01:18:22 +0200

    Powerby text commit

--- resources/views/makes/build.blade.php
+++ resources/views/makes/build.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full lg:max-w-5xl grid grid-cols-4 gap-10  sm:grid-cols-1 mx-auto mt-20">
         <div
         class="border-t border-t-gray-100 md:border-t-none col-span-4 md:col-span-1 px-3 md:px-6  space-y-10 py-6 pt-10 md:border-l border-gray-100 ">
-        <h1 class="font-bold">Welkom bij powerby pagina!.
+        <h1 class="font-bold text-2xl">Welkom bij Powerby pagina!.
         </h1>
         <p> Hier kun je ontdekken hoe deze webapplicatie is ontstaan en welke tools en middelen ik heb gebruikt om het te realiseren. Misschien ben je al bekend met sommige van deze middelen, of wil je ze zelf gaan gebruiken. Lees verder en laat je inspireren!</p>
         <hr>
